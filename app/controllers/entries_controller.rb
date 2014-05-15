882005dfb12c481078b84b1a6cf34e66e4a6cbd3
Sunny Israni <sunny@zoraab.com>
2014-05-14 21:06:03 -0400

    adding json show view as consumptable API

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -15,4 +15,11 @@ class EntriesController < ApplicationController
       end
     end
   end
+  
+  def show
+    @entry = Entry.find(params[:id])
+    respond_to do |format|
+        format.json  { render :json => @entry }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ ZoraabUsers::Application.routes.draw do
   root 'entries#index'
   
   post '/new' => 'entries#new'
+  
+  resources :entries, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
